0973d9536e08bd852a748bcb3b30645554a5045f
Elisandro Santolin <elisandro.santolin@outlook.com>
2023-01-14 09:41:47 -0300

    Carregamento de novos códigos DevMedia

---
+++ totalJogos.js
@@ -0,0 +1,12 @@
+const jogos = [
+    { id: 1, nome: 'Resident Evil 7', preco: 250 },
+    { id: 2, nome: 'God of War', preco: 300 },
+    { id: 3, nome: 'Stronghold Warlords', preco: 280 },
+    ];
+    
+    let total = 0;
+    
+    for (let i = 0; i < jogos.length; i++) {
+        total += jogos[i].preco; //## PEGA O INDICE (incrementado pelo i++) DO ARRAY E SOMA COM O CAMPO PRECO. (id:1 = preco:250) ##//
+    }
+    console.log(total);
