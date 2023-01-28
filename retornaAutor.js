f16b382592ae554efd5ff6bf30f7d7b7805cfdf6
Elisandro Santolin <elisandro.santolin@outlook.com>
2023-01-28 14:39:50 -0300

    Carregamento arquivos exercicios Devmedia

---
+++ retornaAutor.js
@@ -0,0 +1,17 @@
+let livros = [
+    {titulo: 'Tenki no ko', autor: 'Makoto Shinkai'},
+    {titulo: 'Toradora', autor: 'Yuyuko Takemia'},
+    {titulo: 'Kimi no na wa', autor: 'Makoto Shinkai'},
+    {titulo: 'A silent voice', autor: 'Yoshitoki Oima'},
+    {titulo: '5 Centímetros por segundo', autor: 'Makoto Shinkai'}
+];
+
+function verificaAutor(livro) {
+    return (livro.autor == 'Makoto Shinkai');
+}
+
+let livrosMakotoShinkai = livros.filter(verificaAutor);
+
+livrosMakotoShinkai.forEach(livro => {
+    console.log(livro.titulo);
+});
