d2c38822706f814d11668f2d9811d704b8aa7255
Elisandro Santolin <elisandro.santolin@outlook.com>
2023-02-18 21:19:12 -0300

    Feat: arquivo retorna turno dia

---
+++ retornaTurnoDia.js
@@ -0,0 +1,13 @@
+const dataAtual = new Date();
+
+const hora = dataAtual.getHours();
+
+let msg = "";
+if ( hora < 6 || hora >= 18 ) {
+    msg = 'Boa noite!\n';
+} else if (hora >= 6  && hora < 12 ) {
+    msg = 'Bom dia!\n';
+} else{
+    msg = 'Boa tarde!\n';
+}
+console.log(msg);
