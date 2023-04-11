47d67ba9b6c4196583927a85c8b8cfe19cf1d69c
Elisandro Santolin <elisandro.santolin@outlook.com>
2023-04-10 21:02:18 -0300

    Refactor: added switch case logic weekday

--- verificaSexta_13.js
+++ verificaSexta_13.js
@@ -8,7 +8,27 @@ if (diaSemana === 5 && diaMes === 13){
     } else if (diaSemana === 5) {
         console.log('Sextou!!!');
     } else {
-        console.log('Hoje é '+diaSemana+'.');
+    switch(diaSemana){
+        case 1:
+            diaSemana == 1;
+            console.log('Hoje é segunda-feira.');
+            break;
+        case 2:
+            console.log('Hoje é terça-feira');
+            break;
+        case 3:
+            console.log('Hoje é quarta-feira');
+            break;
+        case 4:
+            console.log('Hoje é quinta-feira');
+            break;
+        case 6:
+            console.log('Hoje é sábado');
+            break;
+        case 7:
+            console.log('Hoje é Domingo');
+            break;
+        default:
+            console.log('data não é válida');
     }
-
-    // melhorar código pra retornar o dia da semana por extenso ## implementar usar switch case //
+}
